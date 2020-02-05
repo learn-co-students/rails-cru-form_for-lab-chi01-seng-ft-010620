9701a0f37d63fe7eaa8ce8d98b52a7de12dde60b
Lindsay Mecher <lindsaymecher@gmail.com>
2020-02-05 09:24:49 -0600

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,43 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.create(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name, :artist, :genre))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+
+  end
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
+end
---
+++ db/migrate/20200204225504_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
+      t.references :artist, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
